34bf00f6bbf62242471721963dc3f22da7aebfd0
Sun Cuiling <suncuiling@192.168.0.103>
2023-09-08 11:05:32 +0800

    add k fold validation for creating training and testing set

--- defines.py
+++ defines.py
@@ -3,6 +3,10 @@ from datetime import datetime
 PARAM_SYSTEM_TIME = datetime.now().strftime("%Y%m%d_%H%M%S")
 PARAM_PATH_CARTE = './data/endoscopic/cartesian'
 PARAM_PATH_POLAR = './data/endoscopic/polar'
+PARAM_PATH_SCORES = ''
+
 
 PARAM_IMG_FOLDER = 'image'
 PARAM_MSK_FOLDER = 'label'
+
+PARAM_SPLIT_NUM = 5
