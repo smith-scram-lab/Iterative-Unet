0358de8570437c209c7317d614d8eb1b5b8646fc
Sun Cuiling <suncuiling@192.168.0.103>
2023-09-11 20:09:34 +0800

    add polar and cartesian dominant images into temp folders based on exited dice score

--- defines.py
+++ defines.py
@@ -4,7 +4,8 @@ PARAM_SYSTEM_TIME = datetime.now().strftime("%Y%m%d_%H%M%S")
 PARAM_PATH_CARTE = './data/endoscopic/cartesian'
 PARAM_PATH_POLAR = './data/endoscopic/polar'
 PARAM_PATH_SCORES = './prev-analysis'
-
+PARAM_PATH_TEMP_POLAR = "./temp/polar"
+PARAM_PATH_TEMP_CARTE = "./temp/cartesian"
 
 PARAM_IMG_FOLDER = 'image'
 PARAM_MSK_FOLDER = 'label'
